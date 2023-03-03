11e500de6c230fc8a0ede5433c053ad42bf895c6
Selvanth Arulsamy <110884312+ASelvanth@users.noreply.github.com>
2023-03-03 18:26:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000","https://cerulean-bunny-25ac48.netlify.app"],
+//     credentials: true,
+//   })
+// );
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
